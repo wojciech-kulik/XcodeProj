db12c9c410baf83ca414b7a6b7150759acddf85f
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2026-09-01 12:55:11 +0200

    fix: adjust to latest Xcode format

--- Sources/XcodeProj/Objects/Files/PBXGroup.swift
+++ Sources/XcodeProj/Objects/Files/PBXGroup.swift
@@ -239,9 +239,7 @@ public extension PBXGroup {
         }
         let fileReference = PBXFileReference(
             sourceTree: sourceTree,
-            name: filePath.lastComponent,
             explicitFileType: filePath.extension.flatMap(Xcode.filetype),
-            lastKnownFileType: filePath.extension.flatMap(Xcode.filetype),
             path: path
         )
         projectObjects.add(object: fileReference)
